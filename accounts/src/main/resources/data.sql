47f6f78b21f197b951f4f9f752128f1b63196ddf
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-10-13 00:24:47 -0300

    Correction in the sql scripts for accounts

--- accounts/src/main/resources/data.sql
+++ accounts/src/main/resources/data.sql
@@ -21,5 +21,5 @@ INSERT INTO `customer` (`name`,`email`,`mobile_number`,`create_date`)
  VALUES ('Lucas teste','teste@lucas.com','9876548337',CURDATE());
 
 INSERT INTO `accounts` (`customer_id`, `account_number`, `account_type`, `branch_address`, `create_date`)
- VALUES (1, 186576453, 'Savings', '123 Main Street, New York', CURDATE());
+ VALUES (1, 186576453, 'Savings', '123 main.java.com.lucas.git.CardsApplication Street, New York', CURDATE());
 
